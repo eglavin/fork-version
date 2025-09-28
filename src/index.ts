63b3c86e87f11371085dc473428ee59dbf529ac9
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2025-09-28 21:34:44 +0100

    refactor: expose new functions in code usage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
+export { CommitParser } from "./commit-parser/commit-parser";
+export { filterRevertedCommits } from "./commit-parser/filter-reverted-commits";
+export { createParserOptions, type ParserOptions } from "./commit-parser/options";
+export type {
+	Commit,
+	CommitMerge,
+	CommitRevert,
+	CommitReference,
+	CommitNote,
+} from "./commit-parser/types";
+
 export { ForkConfigSchema } from "./config/schema";
 export type { ForkConfig, Config } from "./config/types";
 export { defineConfig } from "./config/define-config";
 export { getUserConfig } from "./config/user-config";
 
+export { getCommitsSinceTag, type CommitsSinceTag } from "./process/get-commits";
 export {
 	getCurrentVersion,
 	getNextVersion,

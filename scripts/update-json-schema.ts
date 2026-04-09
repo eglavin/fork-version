50c2b0e7c6205c5587dd01c0e939b1c113f5f01e
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2026-04-09 21:52:26 +0100

    refactor: migrate scripts to use built in node typescript support

--- scripts/update-json-schema.ts
+++ scripts/update-json-schema.ts
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-// @ts-check
 
 import { join } from "node:path";
 import { pathToFileURL } from "node:url";
 import { writeFileSync } from "node:fs";
 import z from "zod";
-import { ForkConfigSchema } from "../src/config/schema.js";
-import { clickableLink } from "./utils/clickable-link.js";
+import { ForkConfigSchema } from "../src/config/schema.ts";
+import { clickableLink } from "./utils/clickable-link.ts";
 
 const schemaLocation = join(import.meta.dirname, "..", "schema", `latest.json`);
 const jsonSchema = z.toJSONSchema(ForkConfigSchema);

bf38331e3a29abe5aca4d6d4b93368ab8645858c
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2026-05-06 00:15:24 +0100

    refactor: improve jsdocs details for define functions

--- src/config/define-config.ts
+++ src/config/define-config.ts
@@ -1,7 +1,22 @@
 import type { Config } from "./types";
 
 /**
- * [Fork-Version - Config Properties](https://github.com/eglavin/fork-version/blob/main/README.md#config-properties)
+ * Optional helper function to enable intellisense and type checking when defining a Fork-Version configuration object.
+ *
+ * [Fork-Version - Config Options](https://github.com/eglavin/fork-version/blob/main/docs/Configuration.md#config-options)
+ *
+ * @example
+ * ```ts
+ * // File: fork.config.ts
+ * import { defineConfig } from "fork-version";
+ *
+ * export default defineConfig({
+ *   header: "# Changelog\n\nMy Custom Changelog Header",
+ * });
+ * ```
+ *
+ * @param config The configuration object to be defined.
+ * @returns The same configuration object that was passed in.
  */
 export function defineConfig(config: Config): Config {
 	return config;
